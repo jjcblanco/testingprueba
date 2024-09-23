b4d341f76efb79f0830f4566a2b86b37bb7c8ccb
Laura123Mendonca <lauracmendonca@yahoo.com.ar>
2024-09-23 20:14:28 -0300

    lau

---
+++ lauraMe/Js/vacio.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+    Insertar_registro();
+
+}
+)
+function Insertar_registro(){
+    $(document).on('click','#btn_register', function()
+    {
+        var User = $('#UserName').val();
+        var Email= $('#UserEmail').val();
+    })
+    
+}
