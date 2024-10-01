ce65751289132c4e8bb594961c5720c1b71fb279
HoracioBarral <horacio.barral@hotmail.com>
2024-10-01 19:22:10 -0300

    agregar funcion InsertarRegistro()

--- horacio/mijs.js
+++ horacio/mijs.js
@@ -5,3 +5,20 @@ function arranque()
 }
 
 window.onload = arranque;
+
+$(document).ready(function()
+{
+    Insertar_registro();
+
+}
+)
+
+
+function Insertar_registro(){
+    $(document).on('click','#btn_register', function()
+    {
+        var User = $('#UserName').val();
+        var Email= $('#UserEmail').val();
+    })
+    
+}
