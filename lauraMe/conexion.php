edeaf4c1321e047528d2a054a8645a70d942f5df
Laura123Mendonca <lauracmendonca@yahoo.com.ar>
2024-10-25 17:54:25 -0300

    cambio25/10

--- lauraMe/conexion.php
+++ lauraMe/conexion.php
@@ -1,6 +1,6 @@
 <?php 
 
-        $con = mysqli_connect('localhost','root','','testing');
+        $con = mysqli_connect('localhost','root','','datos_personales');
         if(!$con)
         {
             die (' Please Your Connectino '.mysqli_error());
